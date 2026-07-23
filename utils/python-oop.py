caf3e52f7240470754f6f2c96634ada45f66f157
manisarma <manisarma1401@gmail.com>
2026-07-23 08:15:58 +0530

    feat: python OOP - dunder methods __str__ , __repr__ , type hints

--- utils/python-oop.py
+++ utils/python-oop.py
@@ -1,20 +1,27 @@
 class Driver:
-    def __init__(self,name,phone):
+    def __init__(self,name: str, phone: str) -> None:
         self.name = name
         self.phone = phone
-        self.available = True
+        self.available: bool = True
     
-    def go_offline(self):
-        self.available = False
+    def go_offline(self) -> None:
+        self.available= False
         print(f"{self.name} is offline")
 
-    def go_online(self):
+    def go_online(self) -> None:
         self.available = True
         print(f"{self.name} is available")
     
+    def __str__(self) -> str:
+        return f"Driver({self.name},{self.phone}, available: bool = {self.available})"
+    
+    def __repr__(self) -> str:
+        return f"Driver(name = {self.name!r}, phone = {self.phone!r})"
+    
+    
 
 class BikeDriver(Driver):
-    def __init__(self,name,phone,bike_number):
+    def __init__(self,name : str,phone : str,bike_number : str) -> None:
         super().__init__(name,phone)
         self.bike_number = bike_number
     
@@ -31,3 +38,7 @@ driver3 = BikeDriver("Rahul","8080808080","AP31EW3329")
 print(driver3.bike_number)
 driver3.go_offline()
 print(driver3.available)
+
+driver = Driver("Mani","9999999999")
+print(driver)
+print(repr(driver))
